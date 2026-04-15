cdff2a3e567dc0f764bc2af7c8f313b1224c33f3
Igor Balos <ibalosh@gmail.com>
2026-04-15 18:59:51 +0200

    added changes

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { SignupRequest } from '@courtlane/contracts';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { AuthResponse, SignupRequest } from '@courtlane/contracts';
+import { prisma } from '@courtlane/db';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
-  signup(input: SignupRequest): { message: string } {
-    return { message: 'Hello API' };
+  async signup(input: SignupRequest): Promise<AuthResponse> {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email is already registered.');
+    }
+
+    const passwordHash = await hash(input.password);
+
+    const user = await prisma.user.create({
+      data: {
+        email: input.email,
+        name: input.name,
+        passwordHash,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+
+    return { user };
   }
 }
